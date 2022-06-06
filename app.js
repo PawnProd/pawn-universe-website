3873d6125b4317eb02bd6f78fee5b0637a605a86
Pawn <nathan.prospert@pawnproduction.fr>
2022-06-06 16:06:44 +0200

    Correction responsive mobile/tablette

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+// Récupération des éléments du DOM manipulable
+const hamburgerBtn = document.getElementById('hamburger');
+
+// Clique menu mobile
+hamburgerBtn.addEventListener('click', (evt) => {
+    if(hamburgerBtn.classList.contains('active')) {
+        hamburgerBtn.classList.remove('active')
+    } else {
+        hamburgerBtn.classList.add('active')
+    }
+   
+})
 
 
+// Chargement des particules
 tsParticles.loadJSON("particles", "./presets/particles.json")
 .then((container) => {
     console.log("callback - tsparticles config loaded");
